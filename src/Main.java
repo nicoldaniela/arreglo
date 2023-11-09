9a71a9c155963a8444006a4ca8e45990a3e2aa1a
lorena-21 <110685156+lorena-21@users.noreply.github.com>
2023-11-09 15:01:08 -0500

    arreglo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public class nombre {
+        public static void main(String[] args) {
+            int[] numeros = {1, 2, 3, 4, 5};
+            for (int i = 0; i < numeros.length; i++) {
+                System.out.println(numeros[i]);
+            }
+        }
     }
 }
